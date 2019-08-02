b9d1c14d5a5b7fbdd99d1d1e666fa5d65fcf67c0
Shelley Arnold <shelle.kay@gmail.com>
2019-08-01 20:39:19 -0500

    added visual code connection, added one fetch call

---
+++ StudentExercises/StudentExercises/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace StudentExercises.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string SlackHandle { get; set; }
+    }
+}
